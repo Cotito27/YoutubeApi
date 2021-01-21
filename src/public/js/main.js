6b4524a5dfebb4d3aa9167c0056e54ad0b14ba91
Cotito27 <65867363+Cotito27@users.noreply.github.com>
2021-01-20 20:28:59 -0500

    add load image history

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -139,7 +139,7 @@ $(document).ready(function() {
           </div>
             <div class="info__video__yt">
               <div class="img__video">
-                <img class="b-lazy" src="/img/placeholder-image.png" data-src="${image}" alt="">
+                <img class="img-history" src="/img/placeholder-image.png" data-src="${image}" alt="">
               </div>
               <div class="title__video">
                 ${title}
@@ -163,38 +163,7 @@ $(document).ready(function() {
         </div>`;
       }
       containerHistory.innerHTML = html;
-      var bLazy2 = new Blazy({
-        offset: 40
-        , success: function(element){
-        setTimeout(function(){
-        // We want to remove the loader gif now.
-        // First we find the parent container
-        // then we remove the "loading" class which holds the loader image
-        // var parent = element.parentNode;
-        // // console.log(parent);
-        // parent.className = parent.className.replace(/\bloading\b/,'');
-        }, 200);
-          },error: (err) => {
-            alert(err)
-          },
-      });
-        setTimeout(function() {
-          var bLazy2 = new Blazy({
-            offset: 40
-            , success: function(element){
-            setTimeout(function(){
-            // We want to remove the loader gif now.
-            // First we find the parent container
-            // then we remove the "loading" class which holds the loader image
-            // var parent = element.parentNode;
-            // // console.log(parent);
-            // parent.className = parent.className.replace(/\bloading\b/,'');
-            }, 200);
-              },error: (err) => {
-                alert(err)
-              },
-          });
-        }, 300);
+      declareLoadImg();
       if($('.btn__search__history').html().includes('<i')) return;
       $('.btn__search__history').html(`<i class="fas fa-times"></i>`);
     } else {
@@ -226,7 +195,7 @@ $(document).ready(function() {
           </div>
             <div class="info__video__yt">
               <div class="img__video">
-                <img class="b-lazy" src="/img/placeholder-image.png" data-src="${image}" alt="">
+                <img class="img-history" src="/img/placeholder-image.png" data-src="${image}" alt="">
               </div>
               <div class="title__video">
                 ${title}
@@ -251,38 +220,7 @@ $(document).ready(function() {
         </div>`;
       }
       containerHistory.innerHTML = html;
-      var bLazy2 = new Blazy({
-        offset: 40
-        , success: function(element){
-        setTimeout(function(){
-        // We want to remove the loader gif now.
-        // First we find the parent container
-        // then we remove the "loading" class which holds the loader image
-        // var parent = element.parentNode;
-        // // console.log(parent);
-        // parent.className = parent.className.replace(/\bloading\b/,'');
-        }, 200);
-          },error: (err) => {
-            alert(err)
-          },
-      });
-        setTimeout(function() {
-          var bLazy2 = new Blazy({
-            offset: 40
-            , success: function(element){
-            setTimeout(function(){
-            // We want to remove the loader gif now.
-            // First we find the parent container
-            // then we remove the "loading" class which holds the loader image
-            // var parent = element.parentNode;
-            // // console.log(parent);
-            // parent.className = parent.className.replace(/\bloading\b/,'');
-            }, 200);
-              },error: (err) => {
-                alert(err)
-              },
-          });
-        }, 300);
+      declareLoadImg();
       if($('.btn__search__history').html().includes('<svg')) return;
       $('.btn__search__history').html(`<svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="search" class="svg-inline--fa fa-search fa-w-16" role="img" viewBox="0 0 512 512"><path fill="currentColor" d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path></svg>`);
     }
@@ -566,7 +504,7 @@ $(document).ready(function() {
           </div>
             <div class="info__video__yt">
               <div class="img__video">
-                <img class="b-lazy" src="/img/placeholder-image.png" data-src="${image}" alt="">
+                <img class="img-history" src="/img/placeholder-image.png" data-src="${image}" alt="">
               </div>
               <div class="title__video">
                 ${title}
@@ -591,38 +529,7 @@ $(document).ready(function() {
         </div>`;
       }
       containerHistory.innerHTML = html;
-      var bLazy2 = new Blazy({
-        offset: 40
-        , success: function(element){
-        setTimeout(function(){
-        // We want to remove the loader gif now.
-        // First we find the parent container
-        // then we remove the "loading" class which holds the loader image
-        // var parent = element.parentNode;
-        // // console.log(parent);
-        // parent.className = parent.className.replace(/\bloading\b/,'');
-        }, 200);
-          },error: (err) => {
-            alert(err)
-          },
-      });
-        setTimeout(function() {
-          var bLazy2 = new Blazy({
-            offset: 40
-            , success: function(element){
-            setTimeout(function(){
-            // We want to remove the loader gif now.
-            // First we find the parent container
-            // then we remove the "loading" class which holds the loader image
-            // var parent = element.parentNode;
-            // // console.log(parent);
-            // parent.className = parent.className.replace(/\bloading\b/,'');
-            }, 200);
-              },error: (err) => {
-                alert(err)
-              },
-          });
-        }, 300);
+      declareLoadImg();
       $(this).html(`<svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="search" class="svg-inline--fa fa-search fa-w-16" role="img" viewBox="0 0 512 512"><path fill="currentColor" d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path></svg>`);
     }
   });
@@ -1116,21 +1023,6 @@ $(document).ready(function() {
     }
     
   }
-  var bLazy = new Blazy({
-    offset: 40
-    , success: function(element){
-    setTimeout(function(){
-    // We want to remove the loader gif now.
-    // First we find the parent container
-    // then we remove the "loading" class which holds the loader image
-    // var parent = element.parentNode;
-    // // console.log(parent);
-    // parent.className = parent.className.replace(/\bloading\b/,'');
-    }, 200);
-      },error: (err) => {
-        alert(err)
-      },
-  });
 });
 
 window.onload = function() {
